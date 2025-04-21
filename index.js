a2aa5842e11e78977becb82a9591b26931bdb97e
emyemy89 <emiparaschiv345@gmail.com>
2025-04-21 17:33:55 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log('hello  world');
+
+// can't be reasigned
+const name="Emanuel";
+
+// can change
+let age =12;
+
+//object
+
+let person={
+    name:'Emi',
+    age:'20'
+};
+
+console.log(person);
+console.log(person.name);
+
+
+//arrays
+let selectedColors=['red', 'blue'];
+console.log(selectedColors);
+
+selectedColors[3]=1;
+
+function greet (name){
+    console.log('Hello '+name);
+}
+
+greet('Baietel');
